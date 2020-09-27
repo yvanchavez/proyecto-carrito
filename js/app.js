04628172abc59652714d0df6d53b9d655f74711f
yvan <chavez.yvan@gmail.com>
2020-09-27 11:39:56 -0500

    crud del carrito

---
+++ js/app.js
@@ -0,0 +1,93 @@
+//variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
+
+cargarEventListeners();
+function cargarEventListeners(){
+    listaCursos.addEventListener('click',agregarCurso);
+    carrito.addEventListener('click',eliminarCurso);
+
+    vaciarCarrito.addEventListener('click', () => {
+        articulosCarrito = [];
+        limpiarHTML();
+    });
+}
+
+//Funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+        carritoHTML();
+    }
+    
+}
+
+function eliminarCurso(e) {
+    
+    if(e.target.classList.contains('borrar-curso')){
+        const cursoId = e.target.getAttribute('data-id');
+
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+        carritoHTML();
+    }
+    
+}
+
+function leerDatosCurso(curso) {
+
+    
+    const infoCurso = {
+        imagen : curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id : curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    const existe =articulosCarrito.some(curso => curso.id === infoCurso.id);
+    if(existe){
+        articulosCarrito.map(curso=>{
+            if(curso.id === infoCurso.id){
+                curso.cantidad++;
+            }
+        });
+    }else{
+        articulosCarrito = [...articulosCarrito,infoCurso];
+    }
+
+    
+    
+}
+
+function carritoHTML() {
+
+    limpiarHTML();
+
+    articulosCarrito.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td><img src="${imagen}"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}">X</a>
+            </td>
+        `
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+function limpiarHTML() {
+    //contenedorCarrito.innerHTML = '';
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
